e4460adb288ba2789749b41813b0e613d9e11c09
Aasit Vora <aasitvora1999@gmail.com>
2022-05-16 16:52:16 +0530

    tried changing logo, didn't work:

--- src/App.jsx
+++ src/App.jsx
@@ -241,7 +241,8 @@ const App = ({ isServerInfo }) => {
 
 export const LogoMoralis = () => (
   <div style={{ display: "flex" }}>
-    <svg
+    <img src="https://scontent.fpnq17-1.fna.fbcdn.net/v/t1.18169-9/425682_369044779783626_38077328_n.jpg?_nc_cat=111&ccb=1-6&_nc_sid=09cbfe&_nc_ohc=r8HR78692I4AX8hEURs&_nc_ht=scontent.fpnq17-1.fna&oh=00_AT9s0gS_CUiZu9KjuOGEA-vdU05Vk-JDX4vl0dXQuyVzhQ&oe=62A7E988"></img>
+    {/* <svg
       width="60"
       height="38"
       viewBox="0 0 50 38"
@@ -260,7 +261,7 @@ export const LogoMoralis = () => (
         d="M39.7135 25.1249C37.1094 25.1025 34.9991 27.2127 34.9766 29.8169C34.9542 32.4211 37.0645 34.5313 39.6686 34.5538C41.1503 34.5538 42.5647 33.8578 43.4626 32.6905C43.53 32.6007 43.5973 32.4884 43.6871 32.3986C45.1015 30.221 44.4729 27.3025 42.2953 25.9107C41.532 25.3943 40.634 25.1249 39.7135 25.1249Z"
         fill="#B7E803"
       />
-    </svg>
+    </svg> */}
   </div>
 );
 
